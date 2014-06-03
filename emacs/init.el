8575835f0b955486ce1a53e46f956481bd6bdbe4
Eivind Fonn <evfonn@gmail.com>
2014-06-03 14:40:01 +0200

    Playing around still

--- emacs/init.el
+++ emacs/init.el
@@ -11,11 +11,12 @@
 (require 'key-chord)
 (key-chord-mode 1)
 
-; {{{
-    (require 'evil)
-    (evil-mode t)
-    (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
-; }}}
+(require 'evil)
+(evil-mode t)
+(key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+
+(require 'linum-relative)
+(setq linum-relative-current-symbol "")
 
 ; Color scheme
 
@@ -24,11 +25,26 @@
 (provide 'init-themes)
 (load-theme 'badwolf t)
 
+(global-hl-line-mode t)
+
 ; IDO mode
 
 (ido-mode t)
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
+
+; Scrolling
+
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+(setq mouse-wheel-progressive-speed nil)
+(setq mouse-wheel-follow-mouse 't)
+(setq scroll-step 1)
+(setq scroll-margin 3)
+
+; Tabs
+
+(setq-default indent-tabs-mode nil)
+(setq tab-width 8)
 
 ; Varia
 
@@ -36,5 +52,5 @@
 
 (setq inhibit-startup-message t
       inhibit-startup-echo-area-message t)
-   
+
 (define-key global-map (kbd "RET") 'newline-and-indent)

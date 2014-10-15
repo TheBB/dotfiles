cbaf47545765278d65dd713af4ca50444f35e7bd
Eivind Fonn <evfonn@gmail.com>
2014-10-15 16:55:26 +0200

    Add TeX keywords

--- emacs/init.el
+++ emacs/init.el
@@ -532,10 +532,18 @@
     (setq font-latex-fontify-script nil)
     (add-to-list 'LaTeX-verbatim-environments "minted")
     (setq evil-shift-width 2)
+    (setq font-latex-match-function-keywords
+          '(("definecolor" "{{{")
+            ("usetikzlibrary" "{")))
+    (setq font-latex-match-reference-keywords
+          '(("autoref" "{")))
     (setq font-latex-match-textual-keywords
-          '(("doList" "")
-            ("abstract" "{")
-            ("overview" "{"))
+          '(("abstract" "{")
+            ("overview" "{")
+            ("includegraphics" "[{")
+            ("overview" "{")
+            ("doList" "{")
+            ("challengeList" "{")))
     (setq font-latex-match-warning-keywords
           '(("maketitle" "")))
     ))

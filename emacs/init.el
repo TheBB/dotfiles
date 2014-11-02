b128740107c0a2bd30e458175248488ad8776bc7
Eivind Fonn <evfonn@gmail.com>
2014-11-02 11:45:08 +0100

    Remove duplicate latex keywords

--- emacs/init.el
+++ emacs/init.el
@@ -584,8 +584,7 @@
           '(("maketitle" "")
             ("titlepage" "")
             ("frametitle" "{")
-            ("doList" "{")
-            ("challengeList" "{")))))
+            ("doList" "{")))))
 
 
 ;; Python mode

62cfcbc843a489e3ec4f8c9240a69d3a6446fda8
Eivind Fonn <evfonn@gmail.com>
2015-06-01 19:56:44 +0200

    Exclude ox-gfm... due to breakage

--- emacs/spacemacs.el
+++ emacs/spacemacs.el
@@ -43,7 +43,7 @@
    dotspacemacs-additional-packages '()
 
    ;; A list of packages and/or extensions that will not be install and loaded.
-   dotspacemacs-excluded-packages '()
+   dotspacemacs-excluded-packages '(ox-gfm)
 
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
    ;; are declared in a layer which is not a member of

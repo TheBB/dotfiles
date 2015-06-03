a5f2ad7a50f1a5b74253696c658f662ecb77c157
Eivind Fonn <evfonn@gmail.com>
2015-06-03 08:41:16 +0200

    Issue with ox-gfm fixed

--- emacs/spacemacs.el
+++ emacs/spacemacs.el
@@ -44,7 +44,7 @@
    dotspacemacs-additional-packages '()
 
    ;; A list of packages and/or extensions that will not be install and loaded.
-   dotspacemacs-excluded-packages '(ox-gfm)
+   dotspacemacs-excluded-packages '()
 
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
    ;; are declared in a layer which is not a member of

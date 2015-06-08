2bf72e0616daa2a099802c4035b2dbc8b56426a0
Eivind Fonn <evfonn@gmail.com>
2015-06-08 15:48:00 +0200

    Fix typo

--- emacs/spacemacs.el
+++ emacs/spacemacs.el
@@ -41,7 +41,7 @@
      evil-little-word
      )
 
-   ;; List of additional packages that will be installed wihout being
+   ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages then consider to create a layer, you can also put the
    ;; configuration in `dotspacemacs/config'.

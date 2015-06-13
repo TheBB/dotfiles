d794f2dcdcfdac04f0e73ebf06241eced4cbf5e7
Eivind Fonn <evfonn@gmail.com>
2015-06-13 17:21:12 +0200

    Workaround: add `f` to additional packages

--- emacs/spacemacs.el
+++ emacs/spacemacs.el
@@ -45,7 +45,7 @@
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages then consider to create a layer, you can also put the
    ;; configuration in `dotspacemacs/config'.
-   dotspacemacs-additional-packages '()
+   dotspacemacs-additional-packages '(f)
 
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '()

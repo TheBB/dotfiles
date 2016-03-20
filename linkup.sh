ed0314d0f1eceeacb84e611712b148dd2e5474ef
Eivind Fonn <evfonn@gmail.com>
2016-03-20 22:14:20 +0100

    Link up terminator

--- linkup.sh
+++ linkup.sh
@@ -8,6 +8,7 @@ ln -Tsi "$ABS_PATH/flake8/flake8" ~/.config/flake8
 ln -Tsi "$ABS_PATH/ghci/ghci.conf" ~/.ghc/ghci.conf
 ln -Tsi "$ABS_PATH/git/gitconfig" ~/.gitconfig
 ln -Tsi "$ABS_PATH/systemd/user" ~/.config/systemd/user
+ln -Tsi "$ABS_PATH/terminator/config" ~/.config/terminator/config
 ln -Tsi "$ABS_PATH/vim" ~/.vim
 ln -Tsi "$ABS_PATH/vimperator/vimperatorrc" ~/.vimperatorrc
 ln -Tsi "$ABS_PATH/zsh/zshenv" ~/.zshenv

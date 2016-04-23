d30d44aa97125460adf6609382eeb756fc873b43
Eivind Fonn <evfonn@gmail.com>
2016-04-23 16:15:32 +0200

    Never pick the current wallpaper

--- scripts/wallpapers.py
+++ scripts/wallpapers.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
 from os import getenv, listdir
-from os.path import join
+from os.path import basename, join
 from random import choice
-from subprocess import run
+from subprocess import run, PIPE
 
+res = run(['gsettings', 'get', 'org.gnome.desktop.background', 'picture-uri'], stdout=PIPE)
+res = res.stdout.decode().strip()
+if res[0] == "'" and res[-1] == "'":
+    res = res[1:-1]
+
+current = basename(res)
 root = join(getenv('HOME'), '.wallpapers')
-wallpaper = choice(listdir(root))
+available = [l for l in listdir(root) if l != current]
+wallpaper = choice(available)
 run(['gsettings', 'set', 'org.gnome.desktop.background', 'picture-uri',
      join(root, wallpaper)])
